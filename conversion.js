44859c70f032076bf668465bfae32c961c836526
Subham <bhandarimaiya65@gmail.com>
2024-02-02 09:25:44 +0545

    first commit

---
+++ conversion.js
@@ -0,0 +1,36 @@
+// let num = "18";
+
+// let numConversion = Number(num);
+
+// console.log(typeof numConversion);
+// console.log(numConversion);
+
+////--------------------------------------
+
+let num = "18abc";
+
+let numConversion = Number(num);
+
+// console.log(typeof numConversion);
+// console.log(numConversion);
+
+//---------------------------------------------
+// console.log(+true);/
+
+//-----------------------------------------------
+
+// let heroes = ["superman", "captain america", "spider man", "hulk"];
+
+// console.table(heroes);
+
+let obj1 = {
+  name: "Subham",
+  age: 21,
+  male: true,
+};
+
+// console.table(obj1);
+
+console.log(obj1.name + " is " + obj1.age + " year old.");
+
+console.log(typeof obj1.name);
